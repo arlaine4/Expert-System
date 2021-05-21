4496a267998471c3be4eaf68c60dbafa4b58a698
arlaine4 <arthur.laine99@gmail.com>
2021-05-21 14:54:08 +0200

    docstring

--- parsing.py
+++ parsing.py
@@ -2,11 +2,26 @@ initial_facts = []
 
 
 def check_initial_facts_cond(fact):
+    """
+        :param fact: string
+        :return: Bool wheter the string fact is inside the global initial_facts or not
+
+        returns True if the fact name is inside initial_facts list or False if it isn't
+    """
     global initial_facts
-    return True if fact in initial_facts else False
+    return True if fact.name in initial_facts else False
 
 
 def add_coord_to_class(instance, new_coord):
+    """
+        :param instance (class): class instance we want the coordinates to be updated
+        :param new_coord (int, int): the coordinates to add the instance coordinates list
+                -> new_coord correspond to the x (inside line related) and y(wich line) coordinates
+                   of the new element to add to the class
+        :return: update coordinates list
+
+        Update Fact and Comment classes coordinates
+    """
     instance.coord.append(new_coord)
     return instance.coord
 
@@ -18,9 +33,15 @@ class Parsing:
         # self.content = [Comment, Comment, Comment, Fact, Fact,..., InitialFacts, Comment, Comment, Comment, Query]
 
     def read_input_file(self, file_path):
+        """
+            :param file_path(string) : path to the file that we are going to read information from
+
+            Reading input file to store every line into a list
+        """
         self.parse_content = []
         with open(file_path, "r+") as fd:
             for line in fd:
+                # Skipping empty lines
                 if line:
                     self.parse_content.append(line)
         # Don't forget to remove spaces from the lines
@@ -38,6 +59,11 @@ class Query:
         self.unpack_queries(compact_queries)
 
     def unpack_queries(self, compact_queries):
+        """
+        :param compact_queries: a string where every char is glued to another one
+
+        Where just unpacking the string to a list of chars
+        """
         self.queries = []
         for c in compact_queries:
             self.queries.append(Fact(c, (-1, -1)))
--- utils.py
+++ utils.py
@@ -3,6 +3,11 @@ import sys
 
 
 def parse_args():
+    """
+        :return: ArgumentParser object containing every argument passed to the program
+
+        Basic argument checker
+    """
     options = argparse.ArgumentParser()
     options.add_argument("-i", "--input", help="path to input file")
     args = options.parse_args()
@@ -10,6 +15,11 @@ def parse_args():
 
 
 def check_valid_path(file_path):
+    """
+        :param file_path(string): file path to check
+
+        Just checking the path validity of a file
+    """
     try:
         fd = open(file_path, "r+")
     except FileNotFoundError:
