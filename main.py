0fc5c098b2d3c3daa533af175f982bc907e5fe6f
arlaine4 <arthur.laine99@gmail.com>
2021-05-29 16:52:45 +0200

    evaluate renamed to sub_queries, the sub_queries file is ready to be used from the solving, probably the last commit on this branch

--- main.py
+++ main.py
@@ -1,63 +1,60 @@
 import utils
 import parseur
-import evaluate as eval
-
+import sub_queries as sub_q
 
 if __name__ == "__main__":
-	options = utils.parse_args()
-	utils.check_valid_path(options.input)
-	exsys = parseur.Exsys(options.input)
-	exsys.parsing()
+    options = utils.parse_args()
+    utils.check_valid_path(options.input)
+    exsys = parseur.Exsys(options.input)
+    exsys.parsing()
 
-	exsys.facts.sort(key=lambda x: x.name)
-	exsys.initials.sort(key=lambda x: x.name)
-	exsys.queries.sort(key=lambda x: x.name)
-	for elem in exsys.initials:
-		elem.cond = True
+    exsys.facts.sort(key=lambda x: x.name)
+    exsys.initials.sort(key=lambda x: x.name)
+    exsys.queries.sort(key=lambda x: x.name)
+    for elem in exsys.initials:
+        elem.cond = True
 
-	print(exsys, end="\n\n")
-	print("RPN -------------------- start")
-	for elem in exsys.rpn:
-		print(elem)
-	print("RPN -------------------- end")
+    print(exsys, end="\n\n")
+    print("RPN -------------------- start")
+    for elem in exsys.rpn:
+        print(elem)
+    print("RPN -------------------- end")
 
-	"""
-	----------------------------------------------------
-	RPNs are now correct
-	Under hear I suppose, that you try to think about how do we want to
-	solve stuff form here. I mean basically it "should" be straight forward like
-	any other 'rpn' is solved. Only difference is that we don't have numbers
-	but facts ('True'/'False') and the '!' operator is a bit special.
+    """
+    ----------------------------------------------------
+    RPNs are now correct
+    Under hear I suppose, that you try to think about how do we want to
+    solve stuff form here. I mean basically it "should" be straight forward like
+    any other 'rpn' is solved. Only difference is that we don't have numbers
+    but facts ('True'/'False') and the '!' operator is a bit special.
 
-	The '!' works like this (in my brain):	#the test file 'mhm.txt' has some '!'operators inside
-	input:		A + C => A + !G
-	initials:	=AC
-	queries:	?G
+    The '!' works like this (in my brain):	#the test file 'mhm.txt' has some '!'operators inside
+    input:		A + C => A + !G
+    initials:	=AC
+    queries:	?G
 
-	left:		True + True		gives		True
-	right:		True + !G		MUST be		True
-				True + !False	=			True
-			------->	G = False
+    left:		True + True		gives		True
+    right:		True + !G		MUST be		True
+                True + !False	=			True
+            ------->	G = False
 
-	input:		A + C => A + !G
-	rpn:		A C +  > A G ! +
+    input:		A + C => A + !G
+    rpn:		A C +  > A G ! +
 
-	rpn:		A C +  > A G !   =			#the '!' takes ONLY 1 parameter, the 1 right before
-	rpn:		____.  > A __. + =			#any other operator takes 2 param, the 2 right before
-	rpn:		       > ______. =
+    rpn:		A C +  > A G !   =			#the '!' takes ONLY 1 parameter, the 1 right before
+    rpn:		____.  > A __. + =			#any other operator takes 2 param, the 2 right before
+    rpn:		       > ______. =
 
 
-	for example like this...
-	.------------
-	|
-	|
-	v
-	"""
-	print("Evaluate --------------- start")
-	exsys.rpn.sort(key=len)
-	solving = eval.Evaluate(exsys)
-	solving.evaluate_equation()
-	#for elem in exsys.rpn:
-		#print(elem)
-		#exsys.evaluate(elem)
-		#print()
+    for example like this...
+    .------------
+    |
+    |
+    v
+    """
+    print("Evaluate --------------- start")
+
+    # How to declare the sub_queries instance and call it for one query
+    # exsys.rpn.sort(key=len)
+    # s = sub_q.Evaluate(exsys)
+    # sub = s.evaluate_equation(exsys.queries[0])
---
+++ sub_queries.py
@@ -0,0 +1,93 @@
+import utils
+import copy
+
+
+class Evaluate:
+    def __init__(self, exsys):
+        self.queries = exsys.queries
+        self.initials = exsys.initials
+        self.facts = exsys.facts
+        self.rpn = exsys.rpn
+
+    def update_query_state(self, query):
+        """
+            :param query(Fact instance): query to get sub_queries for
+            :return: list of sub_queries for one main query
+        """
+        lst_eq = []
+        sub_queries = [query]
+        sub_queries, lst_eq = self.get_sub_queries(sub_queries, query, lst_eq, len(sub_queries))
+        if len(sub_queries) > 1:
+            sub_queries.pop(0)
+            new_sub_q = [sub_queries[0]]
+            for i in range(len(sub_queries)):
+                new_sub_q, lst_eq = self.get_sub_queries(sub_queries, sub_queries[i], lst_eq, len(sub_queries))
+            sub_queries = self.update_sub_queries(sub_queries, new_sub_q)
+        return sub_queries
+
+    def update_sub_queries(self, sub, tmp):
+        """
+            :param sub(list of facts): list of sub_queries to be updated
+            :param tmp(list of facts): list of facts to append to sub_queries
+            :return: updated sub_queries
+        """
+        for elem in tmp:
+            if elem not in sub:
+                sub.append(elem)
+                sub[-1].coord = utils.locate_query_inside_rpns(elem.name, self.rpn)
+        return sub
+
+    def get_sub_queries(self, sub_queries, query, lst_eq, index_q):
+        """
+            :param sub_queries(list of facts)	: list of sub_queries for one query, incrementing with the recursion
+            :param query(Fact instance)			: the query we are basing the recursion on, applied on the dynamic
+                                                  sub_queries list aswell
+            :param lst_eq(list of equations)	: where to look for the facts, dynamic list
+            :param index_q(int)					: index inside the sub_queries list, decreasing as we go on the recursion
+            :return								: list of sub_queries ans list of equations
+        """
+        rpn_idx = utils.locate_query_inside_rpns(query.name, self.rpn)
+        if not rpn_idx:
+            return sub_queries, lst_eq
+        lst_eq.append(self.rpn[rpn_idx[0][0]])
+        left_side, operators = utils.unpack_facts_operators(self, lst_eq[-1])
+        undetermined_facts = self.check_solvable_side(left_side)
+        if not undetermined_facts:
+            if utils.check_recursion_coord(rpn_idx, sub_queries):
+                lst_eq.pop(-1)
+            return sub_queries, lst_eq
+        sub_queries = self.update_sub_queries(sub_queries, undetermined_facts)
+        if index_q - 1 > 0:
+            return self.get_sub_queries(sub_queries, sub_queries[index_q], lst_eq, index_q - 1)
+        return sub_queries, lst_eq
+
+    def evaluate_equation(self, query):
+        """
+            :param query(Fact instance): The query we want sub_queries for
+            :return: list of sub_queries for one query
+        """
+        sub_queries = []
+        obj_query = copy.deepcopy(query)
+        sub_queries.append(self.update_query_state(obj_query))
+        return sub_queries
+
+    def get_fact(self, name):
+        """
+            :param name(string): name of the fact you want to access
+            :return wheter the fact instance or None
+        """
+        for elem in self.facts:
+            if elem.name == name:
+                return elem
+        return None
+
+    def check_solvable_side(self, side):
+        """
+            :param side(list): list of facts and operators inside an equation side
+            :return: list of facts whose states are undertermined
+        """
+        lst_undetermined = []
+        for elem in side:
+            if elem.cond is None:
+                lst_undetermined.append(elem)
+        return lst_undetermined
